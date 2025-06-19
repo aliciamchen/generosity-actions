624d2cb47de12eedd97e8e24f78a9d0f0321ab66
Alicia Chen <amqchen@gmail.com>
2025-06-19 23:18:06 +0800

    study 5 and 6 analysis code final

--- analysis/study_5.R
+++ analysis/study_5.R
@@ -3,12 +3,10 @@ library(showtext)
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
-library(knitr)
-library(kableExtra)
-library(tidyboot)
 library(lme4)
 library(emmeans)
 library(lmerTest)
+library(car)
 
 showtext_auto()
 
@@ -80,10 +78,10 @@ main_data <- data %>%
   mutate(
     partner_status = factor(partner_status, levels = c("equal", "lower", "higher")),
     relationship = factor(relationship, levels = c("equal", "unequal")),
-    participant_first_choice = factor(participant_first_choice, levels = c("receive", "give")),
-    partner_first_choice = factor(partner_first_choice, levels = c("receive", "give")),
+    participant_first_choice = factor(participant_first_choice, levels = c("give", "receive")),
+    partner_first_choice = factor(partner_first_choice, levels = c("give", "receive")),
     coordination = factor(coordination, levels = c("False", "True")),
-    participant_second_choice = factor(participant_second_choice, levels = c("receive", "give")),
+    participant_second_choice = factor(participant_second_choice, levels = c("give", "receive")),
     strategy = factor(strategy, levels = c("alternating", "repeating")), 
     first_choices = factor(first_choices, levels = c("give_give", "give_receive", "receive_give", "receive_receive"))
   )
@@ -104,53 +102,45 @@ study_5_demographics %>% summarize(mean_age = mean(age), sd_age = sd(age), min_a
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------------
 main_model <- glmer(
-  strategy_alternating ~ relationship + (1 | scenario_id) + (1 | subject_id),
+  strategy_alternating ~ partner_status * coordination + (1 | scenario_id) + (1 | subject_id),
   data = main_data_filtered,
   family = binomial(link = "logit"), 
   control = glmerControl(optimizer = "bobyqa"))
 summary(main_model)
 
-# Generalized linear mixed model fit by maximum likelihood (Laplace Approximation) ['glmerMod']
-# Family: binomial  ( logit )
-# Formula: strategy_alternating ~ relationship + (1 | scenario_id) + (1 |      subject_id)
-# Data: main_data_filtered
-# Control: glmerControl(optimizer = "bobyqa")
-# 
-# AIC      BIC   logLik deviance df.resid 
-# 1071.2   1090.5   -531.6   1063.2      914 
-# 
-# Scaled residuals: 
-#   Min      1Q  Median      3Q     Max 
-# -1.4739 -0.5956 -0.4594  0.8868  2.1427 
-# 
-# Random effects:
-#   Groups      Name        Variance Std.Dev.
-# subject_id  (Intercept) 1.07     1.034   
-# scenario_id (Intercept) 0.00     0.000   
-# Number of obs: 918, groups:  subject_id, 153; scenario_id, 6
-# 
-# Fixed effects:
-#   Estimate Std. Error z value Pr(>|z|)    
-# (Intercept)   -0.98038    0.12284  -7.981 1.45e-15 ***
-#   relationship1  0.26783    0.08222   3.258  0.00112 ** 
-#   ---
-#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-# 
-# Correlation of Fixed Effects:
-#   (Intr)
-# relatinshp1 0.147 
-# optimizer (bobyqa) convergence code: 0 (OK)
-# boundary (singular) fit: see help('isSingular')
-
-
 ## ------------------------------------------------------------------------------------------------------------------------------------------------------
-emm <- emmeans(main_model, ~ relationship)
+emm <- emmeans(main_model, ~ partner_status * coordination)
+summary(emm, infer = T)
+
+# partner_status coordination emmean    SE  df asymp.LCL asymp.UCL z.ratio p.value
+# equal          False        -1.187 0.218 Inf    -1.615    -0.759  -5.432  <.0001
+# lower          False        -0.925 0.211 Inf    -1.339    -0.511  -4.376  <.0001
+# higher         False        -1.115 0.225 Inf    -1.555    -0.675  -4.963  <.0001
+# equal          True         -0.229 0.211 Inf    -0.642     0.185  -1.083  0.2786
+# lower          True         -1.599 0.250 Inf    -2.088    -1.110  -6.406  <.0001
+# higher         True         -1.476 0.235 Inf    -1.936    -1.015  -6.283  <.0001
+
 pairs(emm)
 
-# contrast        estimate    SE  df z.ratio p.value
-# equal - unequal    0.536 0.164 Inf   3.258  0.0011
+# contrast                   estimate    SE  df z.ratio p.value
+# equal False - lower False   -0.2617 0.271 Inf  -0.965  0.9290
+# equal False - higher False  -0.0717 0.283 Inf  -0.253  0.9999
+# equal False - equal True    -0.9582 0.280 Inf  -3.417  0.0083
+# equal False - lower True     0.4123 0.301 Inf   1.372  0.7442
+# equal False - higher True    0.2891 0.287 Inf   1.008  0.9154
+# lower False - higher False   0.1900 0.277 Inf   0.687  0.9834
+# lower False - equal True    -0.6964 0.270 Inf  -2.580  0.1021
+# lower False - lower True     0.6741 0.300 Inf   2.243  0.2179
+# lower False - higher True    0.5508 0.282 Inf   1.952  0.3704
+# higher False - equal True   -0.8864 0.281 Inf  -3.155  0.0200
+# higher False - lower True    0.4841 0.308 Inf   1.573  0.6166
+# higher False - higher True   0.3608 0.299 Inf   1.207  0.8337
+# equal True - lower True      1.3705 0.300 Inf   4.562  0.0001
+# equal True - higher True     1.2472 0.288 Inf   4.337  0.0002
+# lower True - higher True    -0.1233 0.308 Inf  -0.400  0.9987
 # 
 # Results are given on the log odds ratio (not the response) scale. 
+# P value adjustment: tukey method for comparing a family of 6 estimates 
 
 ## ----secondary-analysis--------------------------------------------------------------------------------------------------------------------------------
 # Model 1: Base model
@@ -161,6 +151,7 @@ model1 <- glmer(
   family = binomial(link = "logit")
 )
 summary(model1)
+
 
 
 # Model 2: Including partner status
@@ -197,27 +188,43 @@ summary(model2)
 # Fixed effects:
 #   Estimate Std. Error z value Pr(>|z|)    
 # (Intercept)                                     -0.07314    0.07515  -0.973  0.33040    
-# partner_first_choice1                            0.85625    0.07515  11.394  < 2e-16 ***
-#   participant_first_choice1                        0.01320    0.07645   0.173  0.86290    
+# partner_first_choice1                           -0.85625    0.07515 -11.394  < 2e-16 ***
+#   participant_first_choice1                       -0.01320    0.07645  -0.173  0.86290    
 # partner_status1                                 -0.05751    0.10121  -0.568  0.56984    
-# partner_status2                                 -0.10474    0.10682  -0.981  0.32681    
+# partner_status2                                 -0.10474    0.10681  -0.981  0.32681    
 # partner_first_choice1:participant_first_choice1 -0.09031    0.07645  -1.181  0.23746    
-# partner_first_choice1:partner_status1           -0.30945    0.10121  -3.058  0.00223 ** 
-#   partner_first_choice1:partner_status2            0.14262    0.10682   1.335  0.18182    
+# partner_first_choice1:partner_status1            0.30945    0.10121   3.058  0.00223 ** 
+#   partner_first_choice1:partner_status2           -0.14262    0.10681  -1.335  0.18182    
 # ---
 #   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 # 
 # Correlation of Fixed Effects:
 #   (Intr) prtn__1 prtc__1 prtn_1 prtn_2 p__1:__ p__1:_1
-# prtnr_frs_1 -0.044                                              
-# prtcpnt_f_1  0.223 -0.047                                       
-# prtnr_stts1 -0.054 -0.017   0.103                               
-# prtnr_stts2 -0.015 -0.034  -0.199  -0.483                       
-# prtn__1:__1 -0.047  0.223  -0.046  -0.032 -0.003                
-# prtnr__1:_1 -0.017 -0.054  -0.032  -0.039  0.053  0.103         
-# prtnr__1:_2 -0.034 -0.015  -0.003   0.053 -0.051 -0.199  -0.483 
+# prtnr_frs_1  0.044                                              
+# prtcpnt_f_1 -0.223 -0.047                                       
+# prtnr_stts1 -0.054  0.017  -0.103                               
+# prtnr_stts2 -0.015  0.034   0.199  -0.483                       
+# prtn__1:__1 -0.047 -0.223   0.046  -0.032 -0.003                
+# prtnr__1:_1  0.017 -0.054  -0.032   0.039 -0.053 -0.103         
+# prtnr__1:_2  0.034 -0.015  -0.003  -0.053  0.051  0.199  -0.483 
 # optimizer (bobyqa) convergence code: 0 (OK)
 # boundary (singular) fit: see help('isSingular')
+
+Anova(model2, type = "III")
+
+# Analysis of Deviance Table (Type III Wald chisquare tests)
+# 
+# Response: participant_second_choice_generous
+# Chisq Df Pr(>Chisq)    
+# (Intercept)                                     0.9473  1   0.330403    
+# partner_first_choice                          129.8196  1  < 2.2e-16 ***
+#   participant_first_choice                        0.0298  1   0.862900    
+# partner_status                                  2.3776  2   0.304581    
+# partner_first_choice:participant_first_choice   1.3956  1   0.237461    
+# partner_first_choice:partner_status             9.3753  2   0.009208 ** 
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
 
 # Compare models using likelihood ratio test
 anova(model1, model2)
--- analysis/study_6.R
+++ analysis/study_6.R
@@ -3,12 +3,10 @@ library(showtext)
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
-library(knitr)
-library(kableExtra)
-library(tidyboot)
 library(lme4)
 library(emmeans)
 library(lmerTest)
+library(car)
 
 showtext_auto()
 
@@ -80,10 +78,10 @@ main_data <- data %>%
   mutate(
     partner_status = factor(partner_status, levels = c("equal", "lower", "higher")),
     relationship = factor(relationship, levels = c("equal", "unequal")),
-    participant_first_choice = factor(participant_first_choice, levels = c("receive", "give")),
-    partner_first_choice = factor(partner_first_choice, levels = c("receive", "give")),
+    participant_first_choice = factor(participant_first_choice, levels = c("give", "receive")),
+    partner_first_choice = factor(partner_first_choice, levels = c("give", "receive")),
     coordination = factor(coordination, levels = c("False", "True")),
-    participant_second_choice = factor(participant_second_choice, levels = c("receive", "give")),
+    participant_second_choice = factor(participant_second_choice, levels = c("give", "receive")),
     strategy = factor(strategy, levels = c("alternating", "repeating")), 
     first_choices = factor(first_choices, levels = c("give_give", "give_receive", "receive_give", "receive_receive"))
   )
@@ -104,53 +102,48 @@ study_6_demographics %>% summarize(mean_age = mean(age), sd_age = sd(age), min_a
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------------
 main_model <- glmer(
-  strategy_alternating ~ relationship + (1 | scenario_id) + (1 | subject_id),
+  strategy_alternating ~ partner_status * coordination + (1 | scenario_id) + (1 | subject_id),
   data = main_data_filtered,
   family = binomial(link = "logit"), 
   control = glmerControl(optimizer = "bobyqa"))
 summary(main_model)
 
-# Generalized linear mixed model fit by maximum likelihood (Laplace Approximation) ['glmerMod']
-# Family: binomial  ( logit )
-# Formula: strategy_alternating ~ relationship + (1 | scenario_id) + (1 |      subject_id)
-# Data: main_data_filtered
-# Control: glmerControl(optimizer = "bobyqa")
-# 
-# AIC      BIC   logLik deviance df.resid 
-# 1105.8   1125.3   -548.9   1097.8      944 
-# 
-# Scaled residuals: 
-#   Min      1Q  Median      3Q     Max 
-# -1.3451 -0.5749 -0.4729  0.8831  2.1145 
-# 
-# Random effects:
-#   Groups      Name        Variance Std.Dev.
-# subject_id  (Intercept) 0.6938   0.833   
-# scenario_id (Intercept) 0.0000   0.000   
-# Number of obs: 948, groups:  subject_id, 158; scenario_id, 6
-# 
-# Fixed effects:
-#   Estimate Std. Error z value Pr(>|z|)    
-# (Intercept)   -0.94956    0.10748  -8.835  < 2e-16 ***
-#   relationship1  0.32084    0.07956   4.033 5.51e-05 ***
-#   ---
-#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-# 
-# Correlation of Fixed Effects:
-#   (Intr)
-# relatinshp1 0.146 
-# optimizer (bobyqa) convergence code: 0 (OK)
-# boundary (singular) fit: see help('isSingular')
-
-
 ## ------------------------------------------------------------------------------------------------------------------------------------------------------
-emm <- emmeans(main_model, ~ relationship)
+emm <- emmeans(main_model, ~ partner_status * coordination)
+summary(emm, infer = T)
+
+# partner_status coordination emmean    SE  df asymp.LCL asymp.UCL z.ratio p.value
+# equal          False        -1.049 0.193 Inf    -1.427    -0.672  -5.442  <.0001
+# lower          False        -1.055 0.202 Inf    -1.451    -0.659  -5.218  <.0001
+# higher         False        -1.291 0.201 Inf    -1.684    -0.898  -6.434  <.0001
+# equal          True         -0.138 0.199 Inf    -0.529     0.253  -0.692  0.4890
+# lower          True         -1.175 0.209 Inf    -1.585    -0.766  -5.626  <.0001
+# higher         True         -1.653 0.245 Inf    -2.133    -1.173  -6.746  <.0001
+# 
+# Results are given on the logit (not the response) scale. 
+# Confidence level used: 0.95 
+
 pairs(emm)
 
-# contrast        estimate    SE  df z.ratio p.value
-# equal - unequal    0.642 0.159 Inf   4.033  0.0001
+# contrast                   estimate    SE  df z.ratio p.value
+# equal False - lower False    0.0056 0.260 Inf   0.022  1.0000
+# equal False - higher False   0.2415 0.258 Inf   0.937  0.9369
+# equal False - equal True    -0.9115 0.264 Inf  -3.453  0.0073
+# equal False - lower True     0.1259 0.263 Inf   0.478  0.9969
+# equal False - higher True    0.6034 0.292 Inf   2.068  0.3040
+# lower False - higher False   0.2359 0.264 Inf   0.894  0.9480
+# lower False - equal True    -0.9171 0.266 Inf  -3.446  0.0075
+# lower False - lower True     0.1203 0.274 Inf   0.439  0.9979
+# lower False - higher True    0.5978 0.298 Inf   2.003  0.3406
+# higher False - equal True   -1.1530 0.264 Inf  -4.361  0.0002
+# higher False - lower True   -0.1156 0.267 Inf  -0.432  0.9981
+# higher False - higher True   0.3619 0.299 Inf   1.211  0.8315
+# equal True - lower True      1.0374 0.272 Inf   3.814  0.0019
+# equal True - higher True     1.5149 0.301 Inf   5.039  <.0001
+# lower True - higher True     0.4774 0.301 Inf   1.588  0.6065
 # 
 # Results are given on the log odds ratio (not the response) scale. 
+# P value adjustment: tukey method for comparing a family of 6 estimates 
 
 ## ----secondary-analysis--------------------------------------------------------------------------------------------------------------------------------
 # Model 1: Base model
@@ -161,6 +154,7 @@ model1 <- glmer(
   family = binomial(link = "logit")
 )
 summary(model1)
+
 
 
 # Model 2: Including partner status
@@ -197,27 +191,43 @@ summary(model2)
 # Fixed effects:
 #   Estimate Std. Error z value Pr(>|z|)    
 # (Intercept)                                      0.01195    0.08047   0.149  0.88192    
-# partner_first_choice1                            0.90833    0.07463  12.170  < 2e-16 ***
-#   participant_first_choice1                        0.15649    0.07640   2.048  0.04053 *  
+# partner_first_choice1                           -0.90833    0.07463 -12.170  < 2e-16 ***
+#   participant_first_choice1                       -0.15649    0.07640  -2.048  0.04053 *  
 #   partner_status1                                  0.04998    0.10137   0.493  0.62199    
 # partner_status2                                 -0.31224    0.10767  -2.900  0.00373 ** 
-#   partner_first_choice1:participant_first_choice1 -0.12204    0.07587  -1.609  0.10771    
-# partner_first_choice1:partner_status1           -0.41571    0.10144  -4.098 4.17e-05 ***
-#   partner_first_choice1:partner_status2            0.09971    0.10790   0.924  0.35544    
+#   partner_first_choice1:participant_first_choice1 -0.12204    0.07587  -1.609  0.10772    
+# partner_first_choice1:partner_status1            0.41571    0.10144   4.098 4.17e-05 ***
+#   partner_first_choice1:partner_status2           -0.09971    0.10790  -0.924  0.35544    
 # ---
 #   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 # 
 # Correlation of Fixed Effects:
 #   (Intr) prtn__1 prtc__1 prtn_1 prtn_2 p__1:__ p__1:_1
-# prtnr_frs_1 -0.007                                              
-# prtcpnt_f_1  0.110 -0.032                                       
-# prtnr_stts1 -0.091 -0.014   0.142                               
-# prtnr_stts2 -0.023 -0.071  -0.222  -0.457                       
-# prtn__1:__1 -0.036  0.121  -0.049  -0.055  0.082                
-# prtnr__1:_1 -0.014 -0.095  -0.050  -0.016  0.090  0.141         
-# prtnr__1:_2 -0.066 -0.029   0.073   0.088 -0.095 -0.227  -0.459 
+# prtnr_frs_1  0.007                                              
+# prtcpnt_f_1 -0.110 -0.032                                       
+# prtnr_stts1 -0.091  0.014  -0.142                               
+# prtnr_stts2 -0.023  0.071   0.222  -0.457                       
+# prtn__1:__1 -0.036 -0.121   0.049  -0.055  0.082                
+# prtnr__1:_1  0.014 -0.095  -0.050   0.016 -0.090 -0.141         
+# prtnr__1:_2  0.066 -0.029   0.073  -0.088  0.095  0.227  -0.459 
 # optimizer (bobyqa) convergence code: 0 (OK)
 # boundary (singular) fit: see help('isSingular')
+
+Anova(model2, type = "III")
+
+# Analysis of Deviance Table (Type III Wald chisquare tests)
+# 
+# Response: participant_second_choice_generous
+# Chisq Df Pr(>Chisq)    
+# (Intercept)                                     0.0221  1  0.8819169    
+# partner_first_choice                          148.1207  1  < 2.2e-16 ***
+#   participant_first_choice                        4.1957  1  0.0405254 *  
+#   partner_status                                  9.2871  2  0.0096236 ** 
+#   partner_first_choice:participant_first_choice   2.5874  1  0.1077154    
+# partner_first_choice:partner_status            17.9537  2  0.0001263 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
 
 # Compare models using likelihood ratio test
 anova(model1, model2)
